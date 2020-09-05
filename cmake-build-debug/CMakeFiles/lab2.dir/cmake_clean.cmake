84fc406cf795de37101baf7d1357510adb34ac69
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2020-09-05 12:43:11 +0530

    git ignored properly

---
+++ cmake-build-debug/CMakeFiles/lab2.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2.dir/src/helper.c.obj"
+  "CMakeFiles/lab2.dir/src/image.c.obj"
+  "CMakeFiles/lab2.dir/src/main.c.obj"
+  "CMakeFiles/lab2.dir/src/matrixmath.c.obj"
+  "CMakeFiles/lab2.dir/src/ppm.c.obj"
+  "lab2.exe"
+  "lab2.exe.manifest"
+  "lab2.pdb"
+  "liblab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
