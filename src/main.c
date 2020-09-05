fbd9bc837f397d2272b8a411a19e9062e7da58bc
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2020-09-05 13:18:54 +0530

    arguments added

--- src/main.c
+++ src/main.c
@@ -5,12 +5,22 @@
 # include"../include/image.h"
 # include"../include/ppm.h"
 
-int main() {
-    PPMImage *ppm = readPPM("C:\\Users\\raghu\\CLionProjects\\lab2\\test\\parrot.ppm");
-    image img = PPMImage2Image(ppm);
-    matrix grey = bgr2grey(img);
-    ppmgrey(img, grey);
-    PPMImage *finalppm = Image2PPMImage(img);
-    writePPM("C:\\Users\\raghu\\CLionProjects\\lab2\\final.ppm", finalppm);
-    return 0;
+int main(int argc, char *argv[]) {
+    if (argc == 2 || argc == 3) {
+        PPMImage *ppm = readPPM(argv[1]);
+        image img = PPMImage2Image(ppm);
+        if (argv[3] != 'T2') {
+            imgblur(img);
+        }
+        if (argv[3] != 'T1') {
+            matrix grey = bgr2grey(img);
+            ppmgrey(img, grey);
+        }
+        PPMImage *finalppm = Image2PPMImage(img);
+        writePPM(argv[2], finalppm);
+        return 0;
+    }
+    else {
+        print_exit("Invalid Number of arguments");
+    }
 }
